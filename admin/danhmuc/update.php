fe9cbebaf2d8b1c4191901e5de839763176a454d
thanhvan181 <nguyenthivan18012000@gmail.com>
2021-11-14 16:24:45 +0700

    crud category

--- admin/danhmuc/update.php
+++ admin/danhmuc/update.php
@@ -1,28 +1,34 @@
 
 
 <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ">
+<?php 
+if(is_array($dm)){
+    extract($dm);
+}
 
+
+?>
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-12">
             <div class="card mb-4">
                 <div class="card-header pb-0">
-                    <h2>Thêm danh muc</h2>
+                    <h2>Cập nhật danh muc</h2>
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="table-responsive p-0">
-                        <form action="index.php?act=adddm" method="post">
+                        <form action="index.php?act=updatedm" method="post">
 
                             <div class="form-group">
                                 <label for="category-title" class="label_title">Title</label>
-                                <input name="cat_title" type="text" class="form-control
-                                ">
+                                <input name="tenloai" type="text" class="form-control" value="<?php if(isset($name) && ($name !="")) echo $name; ?>">
                             </div>
 
 
                             <div class="btn_button">
+                                <input type="hidden" name="id" value="<?php if(isset($id) && ($id > 0)) echo $id; ?>">
 
-                                <input name="add_category" type="submit" class="btn_but">
+                                <input name="capnhat" type="submit" value="Cap nhat" class="btn_but">
                                 
                                 <a class="btn_butn" href="index.php?act=listdm">Danh Sach</a>
                             </div>
--- modal/danhmuc.php
+++ modal/danhmuc.php
@@ -13,4 +13,21 @@ function insert_danhmuc($name){
 
     pdo_execute($sql);
 }
+
+function loadone_danhmuc($id){
+    $sql="select * from category where id=".$id;
+    $dm=pdo_query_one($sql);
+    return $dm;
+}
+
+function delete_danhmuc($id){
+    $sql="delete from category where id=".$id;
+    pdo_execute($sql);
+}
+
+
+function update_danhmuc($id,$tenloai){
+    $sql="update category set name='".$tenloai."' where id=".$id;
+    pdo_execute($sql);
+}
 ?>
